d246676be606a4469bd1fef6d3b4dde7e32966e7
Byankina <70693491+Byankina@users.noreply.github.com>
2020-12-01 16:05:57 +0300

    start

---
+++ lr2/Utils.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+//проверка
+template <typename T>
+T GetCorrectNumber(T max)
+{
+	T x;
+	while ((cin >> x).fail() || x < 0 || x>max)
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "Type number (" << 0 << "-" << max << "):";
+	}
+	return x;
+}
+//удаление
+template <typename T>
+void del(unordered_map <int, T>& t,int id)
+{
+		t.erase(id);
+}
+//поиск максимума
+template <typename T>
+int FindMaxID(const unordered_map<int, T>& t)
+{
+	int MaxID = -100;
+	for (const auto& i : t)
+		if (i.second.set_id() > MaxID)
+			MaxID = i.second.set_id();
+	return MaxID;
+}
